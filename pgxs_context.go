9f1c221ca5d23f4e813d12108f80576f7ee03bb6
Sergey Ilyin <ilin.sergey24@wb.ru>
2026-09-24 17:55:00 +0300

    [PGXS feat] Изменено название функций работающих с контекстом

--- pgxs_context.go
+++ pgxs_context.go
@@ -22,26 +22,26 @@ func FromContextTx(ctx context.Context) (pgx.Tx, bool) {
 	return v, ok
 }
 
-func ContextWithPreHasherKey(ctx context.Context, val Prehasher) context.Context {
+func ContextWithPrehasher(ctx context.Context, val Prehasher) context.Context {
 	return context.WithValue(ctx, ctxPreHasherKey("key_prehasher_id"), val)
 }
 
-func FromContextPreHasherKey(ctx context.Context) (Prehasher, bool) {
+func FromContextPrehasher(ctx context.Context) (Prehasher, bool) {
 	v, ok := ctx.Value(ctxPreHasherKey("key_prehasher_id")).(Prehasher)
 	return v, ok
 }
 
-func ContextWithBucketIDKey(ctx context.Context, val BucketID) context.Context {
+func ContextWithBucketID(ctx context.Context, val BucketID) context.Context {
 	return context.WithValue(ctx, ctxBucketIDKey("key_bucket_id"), val)
 }
 
-func FromContextBucketIDKey(ctx context.Context) (BucketID, bool) {
+func FromContextBucketID(ctx context.Context) (BucketID, bool) {
 	v, ok := ctx.Value(ctxBucketIDKey("key_bucket_id")).(BucketID)
 	return v, ok
 }
 
 func (c *Client) bucketFromContext(ctx context.Context) (BucketID, error) {
-	bucketID, ok := FromContextBucketIDKey(ctx)
+	bucketID, ok := FromContextBucketID(ctx)
 	if ok {
 		if err := bucketID.Validate(c.config.Buckets); err != nil {
 			return 0, fmt.Errorf("bucket from context: %w", err)
@@ -50,7 +50,7 @@ func (c *Client) bucketFromContext(ctx context.Context) (BucketID, error) {
 		return bucketID, nil
 	}
 
-	hasher, ok := FromContextPreHasherKey(ctx)
+	hasher, ok := FromContextPrehasher(ctx)
 	if !ok {
 		return 0, fmt.Errorf("no BucketID or PreHasher in context")
 	}
